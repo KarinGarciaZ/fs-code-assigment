09e300e737c2c4bc5133204712c33d74b494ae3f
Oscar <oscarin962011@hotmail.com>
2021-09-22 18:52:09 -0500

    add new solution using radma

--- cleanse.js
+++ cleanse.js
@@ -1,3 +1,7 @@
+const R = require('ramda');
+
+const { pipe, reject, isNil, map, when, is, filter } = R;
+
 // Given this object...
 const myObj = {
   a: 'foo',
@@ -39,66 +43,75 @@ const myObj = {
   }
 };
 
-// ...refactor this function to omit all `null` and `undefined` values from
-// every object reference in the `myObj` tree. Please note that `myObj` should not be
-// mutated.
+/*...refactor this function to omit all `null` and `undefined` values from
+  every object reference in the `myObj` tree. Please note that `myObj` should not be
+  mutated.*/
 
-const cleanse = a => {
-  if (a === null || a === undefined) return true;
+/* 
+  NOTE: You'll find the best solution at the end.
+  I added the implementation inside IIFEs to avoid reference issues between the different solutions.
+*/
 
-  if(typeof a === 'object') {
-    let aCopy;
-    if (Array.isArray(a)) {
-      aCopy = [...a];
-      aCopy.forEach((item, index) => {
-        const result = cleanse(item);
-        if (result === true) aCopy.splice(index, 1);
-        else if (typeof result === 'object') aCopy[index] = result;
-      });
-    } else {
-      aCopy = {...a};
-      Object.keys(aCopy).forEach((k) => {
-        const result = cleanse(aCopy[k]);
-        if (result === true) delete aCopy[k];
-        else if (typeof result === 'object') aCopy[k] = result;
-      });
+// First solution using recursion without external libraries
+(() => {
+  const cleanse = a => {
+    if (a === null || a === undefined) return true;
+
+    if(typeof a === 'object') {
+      let aCopy;
+      if (Array.isArray(a)) {
+        aCopy = [...a];
+        aCopy.forEach((item, index) => {
+          const result = cleanse(item);
+          if (result === true) aCopy.splice(index, 1);
+          else if (typeof result === 'object') aCopy[index] = result;
+        });
+      } else {
+        aCopy = {...a};
+        Object.keys(aCopy).forEach((k) => {
+          const result = cleanse(aCopy[k]);
+          if (result === true) delete aCopy[k];
+          else if (typeof result === 'object') aCopy[k] = result;
+        });
+      }
+
+      return aCopy;
     }
 
-    return aCopy;
-  }
+    return false;
+  };
 
-  return false;
-};
+  console.log(cleanse(myObj));
+})();
+
+// This was my first approach to Ramda since I've never used it before. Here I tried to follow my recursion implemented since the first time.
+(() => {
+  const needsFilter = n => !isNil(n);
+  const loopStructure = n => (typeof n === 'object')? cleanse(n): n;
+
+  const cleanse = a => {
+    const filtered = filter(needsFilter, a);
+    return map(loopStructure, filtered);
+  };
+
+  console.log(cleanse(myObj));
+})();
 
 
+// I continued reading the documentation and found there are aleady logics done. I added pipe, when and is.
+// Here I did the function in one line, but still needed the filter fuction.
+(() => {
+  const needsFilter = n => !isNil(n);
 
-// Algorithm explained. Please note I used recursion to solve this challenge.
-// I wanted to use recursion since the function gets executed just once per property, array or object.
-// So, the time complexity is O(n) (I have 2 forEach inside but it's to read the whole object just once in a linear way).
-const cleanseExplained = a => {
-  // return true if I want to clear this current value
-  if (a === null || a === undefined) return true;
+  const cleanse = a => pipe(filter(needsFilter), map(when(is(Object), cleanse)))(a);
 
-  if(typeof a === 'object') {
-    let aCopy;
-    if (Array.isArray(a)) {
-      aCopy = [...a];
-      aCopy.forEach((item, index) => {
-        const result = cleanse(item); // send every single item in the array into this function
-        if (result === true) aCopy.splice(index, 1);// remove null and undefined values from the array
-        else if (typeof result === 'object') aCopy[index] = result; //assing new values updated (in case of an object)
-      });
-    } else {
-      aCopy = {...a};
-      Object.keys(aCopy).forEach((k) => {
-        const result = cleanse(aCopy[k]);
-        if (result === true) delete aCopy[k];// delete the property if null or undefined
-        else if (typeof result === 'object') aCopy[k] = result; //assing new values updated (in case of an object)
-      });
-    }
+  console.log(cleanse(myObj));
+})();
 
-    return aCopy;//return value updated
-  }
 
-  return false; // return false in case of no update needed.
-};
+// Here I finally did it in one line replicing the filter for reject.
+(() => {
+  const cleanse = a => pipe(reject(isNil), map(when(is(Object), cleanse)))(a);
+
+  console.log(cleanse(myObj));
+})();
